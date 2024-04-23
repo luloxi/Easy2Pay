d1d860ca6123e4530545e0b041628907be3a3545
Lulox <lulox.eth@protonmail.com>
2024-04-23 12:15:16 -0300

    Added 2 tests

--- packages/foundry/test/Easy2PayTest.t.sol
+++ packages/foundry/test/Easy2PayTest.t.sol
@@ -7,8 +7,9 @@ import {Easy2Pay} from "../contracts/Easy2Pay.sol";
 contract Easy2PayTest is Test {
     Easy2Pay easy2pay;
 
-    address USER1 = makeAddr("user1");
-    address USER2 = makeAddr("user2");
+    address USER1 = makeAddr("1");
+    address USER2 = makeAddr("2");
+    address USER3 = makeAddr("3");
     uint256 constant REQUEST_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 1 ether;
 
@@ -16,6 +17,7 @@ contract Easy2PayTest is Test {
         easy2pay = new Easy2Pay();
         vm.deal(USER1, STARTING_BALANCE);
         vm.deal(USER2, STARTING_BALANCE);
+        vm.deal(USER3, STARTING_BALANCE);
     }
 
     function testExactPaymentFulfill() public {
@@ -32,5 +34,22 @@ contract Easy2PayTest is Test {
         vm.prank(USER2);
         vm.expectRevert();
         easy2pay.pay{value: REQUEST_VALUE - 1}(0);
+    }
+
+    function testCantPayForACompletedPayment() public {
+        vm.prank(USER1);
+        easy2pay.requestPayment(uint248(REQUEST_VALUE), USER2, "Test");
+        vm.prank(USER2);
+        easy2pay.pay{value: REQUEST_VALUE}(0);
+        vm.expectRevert();
+        easy2pay.pay{value: REQUEST_VALUE}(0);
+    }
+
+    function testRevertsIfNotPayer() public {
+        vm.prank(USER1);
+        easy2pay.requestPayment(uint248(REQUEST_VALUE), USER2, "Test");
+        vm.prank(USER3);
+        vm.expectRevert();
+        easy2pay.pay{value: REQUEST_VALUE}(0);
     }
 }
